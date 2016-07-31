b72eea264ac7ec3d1a31df6031e2467dc2580061
michael <michaelvdlnz@gmail.com>
2016-07-31 19:55:26 +1200

    Add files via upload

---
+++ aboutUsAppController.js
@@ -0,0 +1,17 @@
+app.controller("aboutUsAppController", ["$scope", function ($scope) {
+    "use strict";
+    $scope.therapist = [
+        {
+            selfie: "Image/Julie%20Routier%202015-09-08%2013.26.32-1.jpg",
+            name: "Julie Routier",
+            owner: "Owner",
+            profession: "Massage Therapist",
+            presure: "(medium to deep pressure)",
+            specific01: "-Energetic Healer",
+            specific02: "-Reiki practitioner ",
+            specific03: "-World Organics Skincare and",
+            specific04: "makeup consultant",
+            avaibility: "Available from Wednesday to Saturday"
+        }
+    ];
+}]);
